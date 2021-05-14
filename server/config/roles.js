87d0d8f010fcbf0373621ebc104ccf7434e1c837
Victor Adams <kysper50@gmail.com>
2021-05-14 16:31:25 -0500

    pushing other files to github

--- server/config/roles.js
+++ server/config/roles.js
@@ -3,8 +3,8 @@ mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
 
-db.user = require("../../models/User");
-db.role = require("../../models/Role");
+db.user = require("../models/User");
+db.role = require("../models/Role");
 
 db.ROLES = ["user", "admin", "moderator"];
 module.exports = rolesConfig;
